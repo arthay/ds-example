da88a0bd57136124323c8e5de4978a8544a97ff5
arthay <arthur15251@gmail.com>
2023-08-09 08:19:07 +0400

    gh action

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-acme`
+  // This tells ESLint to load the config from the package `@dsexample/eslint-config-acme`
   extends: ["acme"],
   settings: {
     next: {
